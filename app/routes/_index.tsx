d842c88a976a06e3f21440e29748b4393bf84b7a
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-02-10 12:58:00 +0530

    moving from ws to socket.io

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { Link, redirect } from "@remix-run/react";
+import { redirect } from "@remix-run/react";
 import { useState } from "react";
 import { authenticator } from "~/utils/auth.server";
 
@@ -17,22 +17,9 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const [userName, setUserName] = useState("");
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex-col">
-        <div>Enter your name please!</div>
-        <input
-          className="block"
-          placeholder="Chikna"
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-        />
-        <Link to={`/chat/${userName}`} className="text-lg">
-          Get Started
-        </Link>
-      </div>
+      {"User logged in!"}
     </div>
   );
 }
